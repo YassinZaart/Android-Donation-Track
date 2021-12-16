8f2f06a14d21aadae375ed18b33a2d9de14e4c3e
YassinZaart <zaart45@gmail.com>
2021-12-17 00:48:47 +0200

    add states

--- app/src/main/java/com/yaslau/myapplication/states/LoginState.kt
+++ app/src/main/java/com/yaslau/myapplication/states/LoginState.kt
@@ -1,4 +1,5 @@
 package com.yaslau.myapplication.states
 
 enum class LoginState {
+    WRONG_PASSWORD, INVALID_EMAIL, SUCCESS
 }
---
+++ app/src/main/java/com/yaslau/myapplication/states/SignUpState.kt
@@ -0,0 +1,5 @@
+package com.yaslau.myapplication.states
+
+enum class SignUpState {
+    SUCCESS, USER_EXISTS
+}
