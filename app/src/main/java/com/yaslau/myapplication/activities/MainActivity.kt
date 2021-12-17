e751e5b2171dc3db0690da0f06b1df1d35069522
YassinZaart <zaart45@gmail.com>
2021-12-17 02:09:41 +0200

    connect the login UI with with the account manager

--- app/src/main/java/com/yaslau/myapplication/activities/MainActivity.kt
+++ app/src/main/java/com/yaslau/myapplication/activities/MainActivity.kt
@@ -3,20 +3,23 @@ package com.yaslau.myapplication.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.yaslau.myapplication.R
-import com.yaslau.myapplication.data.MessageDataClass
-import com.yaslau.myapplication.repository.RetrofitInstance
+import com.yaslau.myapplication.states.LoginState
+import com.yaslau.myapplication.util.AccountManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import retrofit2.Response
-import java.io.IOException
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var emailText: EditText
+    lateinit var passwordText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +27,8 @@ class MainActivity : AppCompatActivity() {
         signUpText.setOnClickListener { signUp() }
         val loginButton : Button = findViewById(R.id.loginButton)
         loginButton.setOnClickListener { login() }
+        emailText = findViewById(R.id.emailEditText)
+        passwordText = findViewById(R.id.passwordEditText)
 
     }
 
@@ -33,20 +38,41 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun login(){
+        val accountManager = AccountManager(this)
+        CoroutineScope(Dispatchers.IO).launch {
+            val state =
+                accountManager.login(emailText.text.toString(), passwordText.text.toString())
+            withContext(Dispatchers.Main) {
 
-            CoroutineScope(Dispatchers.IO).launch {
-                val response = RetrofitInstance.api.getSignInMessage("ddd", "ddd")
-                if(response.isSuccessful){
-                    Log.i("fsuccess", "Successful")
-                }
-                else {
-                    Log.i("fsuccess", "Not Successful")
-                    Log.i("fbody", response.code().toString())
+                when (state) {
+                    LoginState.WRONG_PASSWORD -> Toast.makeText(
+                        applicationContext,
+                        "Wrong Password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    LoginState.INVALID_EMAIL -> Toast.makeText(
+                        applicationContext,
+                        "Invalid Email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    LoginState.SUCCESS -> {
+                        Toast.makeText(
+                            applicationContext,
+                            "Logged in!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val myIntent = Intent(this, SignUpActivity::class.java)
+                        startActivity(myIntent)
+                    }
                 }
             }
 
-       // val myIntent = Intent(this, HomePageActivity::class.java)
-        //startActivity(myIntent)
+        }
+
+    }
+
+    private fun Intent(coroutineScope: CoroutineScope, java: Class<SignUpActivity>): Intent {
+        return Intent(this, HomePageActivity::class.java)
     }
 
 }
